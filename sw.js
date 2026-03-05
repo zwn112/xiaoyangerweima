57cd139f491f8682bc6a3354aa904890bfd1b307
zwn112 <131132257+zwn112@users.noreply.github.com>
2026-03-05 21:42:45 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'xiaoyang-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/icon-180.png', '/icon-32.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+});
